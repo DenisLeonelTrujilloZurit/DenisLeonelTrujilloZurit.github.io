c93299e1dfb01ec4e35631bf264ca88ea185f4f9
DenisLeonelTrujilloZurit <151604223+DenisLeonelTrujilloZurit@users.noreply.github.com>
2023-11-21 01:29:57 -0400

    Add files via upload

---
+++ galeria.js
@@ -0,0 +1,10 @@
+let edad=prompt('¿Cuantos años tienes?');
+if(edad>=4 && edad<=14){
+    alert('¡GENIAL! puedes tomar este curso')
+}else if(edad==15){
+    alert('¡GENIAL! tienes estos 2 cursos que puedes tomar')
+}else if(edad>=16 && edad<=20){
+    alert('¡GENIAL! puedes tomar este curso')
+}else if(edad>20){
+    alert('¡GENIAL! puedes tomar este curso')
+}
